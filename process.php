6a8a368efbf20789d93932d9b3be7226b36e0a0f
tsganey93 <tsganey93@gmail.com>
2016-09-24 14:45:50 +0700

    first commit on github

---
+++ process.php
@@ -0,0 +1,9 @@
+<?php
+$user_name=$_POST["email"];
+$password =$_POST["password"];
+
+if($user_name =="chan" && password=="123456"){
+	echo "đăng nhập thành công"}
+else {
+	echo "dmm sai cmnr"
+};
---
+++ test1.php
@@ -0,0 +1,17 @@
+
+
+<html>
+	<head> 
+	<meta http-equiv="content-type" content="text/html; charset=UTF8">
+	</head>
+	<body>
+		<form name="main_form" method="post" action="process.php">			
+			<input type="text" name="email"/>
+			<p>
+			<input type="password" name="password"/>
+			</p>
+			<p><input type="submit" value="submit form"/></p>
+			</div>
+		</form>
+	</body>
+<html>
