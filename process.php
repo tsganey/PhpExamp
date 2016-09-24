eb6a01d500b213cbbb0be8ae88446ad2090ac423
tsganey93 <tsganey93@gmail.com>
2016-09-24 15:01:53 +0700

    Fix some syntax error

--- process.php
+++ process.php
@@ -2,8 +2,8 @@
 $user_name=$_POST["email"];
 $password =$_POST["password"];
 
-if($user_name =="chan" && password=="123456"){
-	echo "đăng nhập thành công"}
+if($user_name = "chan" && $password = "123456"){
+	echo "đăng nhập thành công"; }
 else {
-	echo "dmm sai cmnr"
-};
+	echo "dmm sai cmnr";
+	}
